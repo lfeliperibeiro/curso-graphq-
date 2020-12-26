1b6b202b778459514861d6117fc51622224db569
Felipe Ribeiro <piporibeiro@gmail.com>
2020-12-26 19:50:26 -0300

    feat: create new query and resolvers

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -2,16 +2,65 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
     # Pontos de entrada da API
-    type Query {
+    scalar Date
+    
+    type Usuario {
+        id: ID
         nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+    
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+    
+    type Query {
+        horaAtual: Date
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
     }
 `
 
 const resolvers = {
-    Query: {
-        nome() {
-            return 'Luiz Felipe'
+    Produto: {
+        precoComDesconto(parent) {
+            return parent.preco * (1 - parent.desconto)
+
+        },
+    },
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
         }
+    },
+    Query: {
+        horaAtual() {
+            return `A hora atual é ${new Date().getHours()}:${new Date().getMinutes()}`
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Carlos',
+                email: 'email@email.com',
+                idade: 32,
+                salario_real: 3500.00,
+                vip: true
+            }
+        },
+        produtoEmDestaque(){
+            return{
+                nome: 'Compultador',
+                preco: 5000.00,
+                desconto: 0.18
+            }
+        }
+
     }
 };
 
