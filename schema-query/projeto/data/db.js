f31897f0582c032274755a0810a13e368cdde2b7
Felipe Ribeiro <piporibeiro@gmail.com>
2021-01-01 18:52:30 -0300

    feat: create enum

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -8,25 +8,29 @@ const usuarios = [{
     nome: 'joao',
     email: 'joao@email.com',
     idade: 37,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 },{
     id: 2,
     nome: 'paulo',
     email: 'paulo@email.com',
     idade: 28,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 },{
     id: 3,
     nome: 'ana',
     email: 'ana@email.com',
     idade: 25,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
 },{
     id: 4,
     nome: 'carla',
     email: 'carla@email.com',
     idade: 21,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'ATIVO'
 }]
 
 module.exports ={ usuarios, perfis}
