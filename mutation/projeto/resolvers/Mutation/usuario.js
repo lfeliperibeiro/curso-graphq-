d97c77a526f8dc4d229f308900a8364edf2395e2
Felipe Ribeiro <piporibeiro@gmail.com>
2021-01-31 20:34:30 -0300

    "refactor: change folders"

--- mutation/projeto/resolvers/Mutation/usuario.js
+++ mutation/projeto/resolvers/Mutation/usuario.js
@@ -1,16 +1,16 @@
-const { usuarios, proximoId } = require('../data/db');
+const { usuarios, proximoId } = require('../../data/db');
 
 function indiceUsuario(filtro) {
     if(!filtro) return -1
     const {id, email} = filtro
-   if(id) {
-       return usuarios
-           .findIndex(u => u.id === id)
-   }else if(email) {
-       return usuarios
-           .findIndex(u => u.email === email)
-   }
-   return -1
+    if(id) {
+        return usuarios
+            .findIndex(u => u.id === id)
+    }else if(email) {
+        return usuarios
+            .findIndex(u => u.email === email)
+    }
+    return -1
 }
 
 module.exports = {
@@ -22,15 +22,15 @@ module.exports = {
             throw new Error('email cadastrado')
         }
 
-    const novo = {
-        id: proximoId(),
-        ...dados,
-        perfil_id: 1,
-        status: 'ATIVO'
-    }
-    usuarios.push(novo)
+        const novo = {
+            id: proximoId(),
+            ...dados,
+            perfil_id: 1,
+            status: 'ATIVO'
+        }
+        usuarios.push(novo)
         return novo
-  },
+    },
     excluirUsuario(_, { filtro }){
         const i = indiceUsuario(filtro)
         if( i < 0) return null
