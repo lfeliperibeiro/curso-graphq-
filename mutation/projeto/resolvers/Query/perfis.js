0fee28be9b3d2db078497a5e6a7a86b149e87dec
Felipe Ribeiro <piporibeiro@gmail.com>
2021-01-31 21:23:28 -0300

    "feat: create perfil crud"

--- mutation/projeto/resolvers/Query/perfis.js
+++ mutation/projeto/resolvers/Query/perfis.js
@@ -10,11 +10,3 @@ module.exports = {
         return sels ? sels[0] : null
     }
 }
-
-module.exports = {
-    perfil(usuario) {
-        const sels = perfis
-            .filter(p => p.id === usuario.perfil_id)
-        return sels ? sels[0] : null
-    }
-}
